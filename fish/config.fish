b7eceebc7f7f53cf577c80983dd80b5992993a38
Cody Frazer <frazercr@gmail.com>
2017-05-12 19:08:58 -0400

    Add main fish config file

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+# rbenv
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
+status --is-interactive; and source (rbenv init -|psub)
+
+# base16 shell
+if status --is-interactive
+  eval sh $HOME/code/dotfiles/base16-builder-ruby/templates/shell/scripts/base16-onedark.sh
+end
--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 BASEDIR=$HOME/code/dotfiles
 
-ln -fsv $BASEDIR/fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
+# git
 ln -fsv $BASEDIR/git/gitconfig $HOME/.gitconfig
 ln -fsv $BASEDIR/git/gitignore $HOME/.gitignore
+
+# fish
+ln -fsv $BASEDIR/fish/config.fish $HOME/.config/fish/config.fish
+ln -fsv $BASEDIR/fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
