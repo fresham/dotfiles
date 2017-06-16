0cbfaa6749e3de67ae13494669207c5498a2e12a
Cody Frazer <frazercr@gmail.com>
2017-06-15 22:37:44 -0400

    Adjust install script to add fish directory

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ ln -fsv $BASEDIR/git/gitconfig $HOME/.gitconfig
 ln -fsv $BASEDIR/git/gitignore $HOME/.gitignore
 
 # fish
+mkdir -p $HOME/.config/fish/functions
 ln -fsv $BASEDIR/fish/config.fish $HOME/.config/fish/config.fish
 ln -fsv $BASEDIR/fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
 
