f7f41b51478ef171fe1b853c79b6aa5c1087ea64
Cody Frazer <frazercr@gmail.com>
2019-07-25 22:35:02 -0400

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,13 @@ status --is-interactive; and source (rbenv init -|psub)
 if status --is-interactive
   eval sh $HOME/code/dotfiles/base16-builder-ruby/templates/shell/scripts/base16-onedark.sh
 end
+
+# personal scripts
+set PATH $HOME/code/bin $PATH
+
+# node module scripts
+set PATH ./node_modules/.bin $PATH
+
+set -Ux EDITOR 'vim -f'
+
+[ -e $HOME/.config/fish/secrets.fish ]; and . $HOME/.config/fish/secrets.fish
