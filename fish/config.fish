cf037c9b02949af972b3c597ece5aba7c9e57464
Cody Frazer <frazercr@gmail.com>
2019-12-19 10:32:16 -0600

    Misc update

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 # rbenv
-status --is-interactive; and source (rbenv init -|psub)
+# status --is-interactive; and source (rbenv init -|psub)
 
 # base16 shell
 if status --is-interactive
@@ -15,3 +15,13 @@ set PATH ./node_modules/.bin $PATH
 set -Ux EDITOR 'vim -f'
 
 [ -e $HOME/.config/fish/secrets.fish ]; and . $HOME/.config/fish/secrets.fish
+
+nvm use 6.10.3
+
+
+set SVN_EDITOR /usr/bin/env vim
+set JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Versions/Current/Home
+set M2_HOME /usr/local/Cellar/maven/3.6.2/libexec
+set M2 $M2_HOME/bin
+set PATH $M2:$PATH
+set MAVEN_OPTS "-Xms512m -Xmx1024m"
